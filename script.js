69d8dbe7bdf3f4a91a38352140364a459637d90f
Jefferson <jeffersonformigoni86@gmail.com>
2024-08-31 16:55:12 -0300

    Add function try catch para capturar o erro e fazer com que a class show-result aparece e suma quando clicar para acionar o evento

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const EUR = 5.32
 const GBP = 6.08
 
 // Obtendo os elementos do formulário.
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -33,10 +34,17 @@ form.onsubmit = (event) => {
 }
 
 // Função para converter a moeda.
-
 function convertCurrency(amount, price, symbol) {
-   console.log(amount, price, symbol)
-
+   try {
+      // Aplica a classe que exibe o footer para mostrar o resultado.
+      footer.classList.add("show-result")
+   } catch (error) {
+      // Remove a classe do footer removendo ele da tela.
+      footer.classList.remove("show-result")
+      
+      console.log("error")
+      alert("Não foi possivel converter. Tente novamente mais tarde...")
+   }
 }
 
 
