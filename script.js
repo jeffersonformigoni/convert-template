c15224f85988ca0cd314367771c1fbe1c38f7c69
Jefferson <jeffersonformigoni86@gmail.com>
2024-08-31 17:14:00 -0300

    Add interpolação e textcontent para aparecer o resultado correto da conversão com cada simbolo e valores

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const USD = 4.87
 const EUR = 5.32
 const GBP = 6.08
 
-// Obtendo os elementos do formulário.
+// Obtendo os elementos do formulário. (Elementos selecionados)
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -36,12 +37,16 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
    try {
+      description.textContent = `${symbol} 1 = ${price}`
+
+
       // Aplica a classe que exibe o footer para mostrar o resultado.
       footer.classList.add("show-result")
+
    } catch (error) {
       // Remove a classe do footer removendo ele da tela.
       footer.classList.remove("show-result")
-      
+
       console.log("error")
       alert("Não foi possivel converter. Tente novamente mais tarde...")
    }
